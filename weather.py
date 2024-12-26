7a3c43008f8ab1ea2de029853bf621da7ceae1d2
Your Name <your.email@example.com>
2024-12-26 14:47:53 +0530

    Added search bar with entry field

--- weather.py
+++ weather.py
@@ -8,8 +8,17 @@ import requests
 import pytz
 
 root = Tk()
-root.title("Misha's Weather App :) ")
+root.title("Sweater Weather - Weather App using TKinter")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
+
+#search box
+Search_image=PhotoImage(file="search_bar.png")
+myimage=Label(image=Search_image)
+myimage.place(x=20,y=20)
+
+textfield = tk.Entry(root,justify="center",width=15,font=("poppins",25,"bold"))
+textfield.place(x=40,y=50)
+
 
 root.mainloop()
