365133bd128e67d368233dfa70226d0edc5818ff
Your Name <your.email@example.com>
2024-12-27 16:05:22 +0530

    added API

--- weather.py
+++ weather.py
@@ -35,7 +35,14 @@ def getWeather():
     json_data = requests.get(api).json()
     condition = json_data['weather'][0]['main']
     description = json_data['weather'][0]['description']
-    
+    temp = int(json_data['main']['temp']-273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+
+    t.config(text=(temp,"°"))    
+    c.config(text=(condition,"","FEELS","LIKE",temp,"°"))
+
 
 
 #search box
